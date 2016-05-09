b640324df9913b8b501cb5728e792d6d5e9f5a14
Lutfun Nahar <lutfun@therapservices.net>
2016-05-09 13:54:16 +0600

    Added feature to send mail

---
+++ src/main/java/net/quizz/repository/MailService.java
@@ -0,0 +1,26 @@
+package net.quizz.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lutfun
+ * @since 5/8/16
+ */
+@Service
+public class MailService {
+
+    @Autowired
+    private MailSender mailSender;
+
+    @Autowired
+    private SimpleMailMessage mailMessage;
+
+    public void sendMail(String subject, String text) {
+        mailMessage.setSubject(subject);
+        mailMessage.setText(text);
+        mailSender.send(mailMessage);
+    }
+}
